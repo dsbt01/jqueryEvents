bf2275cec0ba1d31df648ba8afb230b3019d5eee
David Suarez <dsuarez001@att.net>
2022-10-07 18:28:25 -0400

    ex 3

--- exercises/03-sum-values/index.js
+++ exercises/03-sum-values/index.js
@@ -6,4 +6,8 @@ window.calculateSumListener = function() {
 	var stringB = document.getElementById("secondNumber").value;
 
 	//your code goes here
+	var total = parseInt(stringA) + parseInt(stringB);
+
+	document.getElementById("resultNumber").value = total;
+
 };
