f59b3c72cbbd4a26985653d7cdf5b530cc53118a
David Suarez <dsuarez001@att.net>
2022-10-07 18:46:56 -0400

    ex 6

--- exercises/06-add-listener-with-js/index.js
+++ exercises/06-add-listener-with-js/index.js
@@ -1,6 +1,14 @@
 window.onload = function myLoadFunction() {
 	alert("The website just finished loading!");
 	//some code here
+	let greenButton = document.getElementById("theGreen");
+	greenButton.addEventListener("click", handleGreenButtonClick, true);
+
+	
 };
 
 //the listener function here
+function handleGreenButtonClick()
+{
+	alert("woohoo!");
+}
